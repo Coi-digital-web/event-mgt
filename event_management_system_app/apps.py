f5e5b36ef16897a5880d9f2546a15a48b4866ecd
Coi-digital-web <none>
2024-09-09 18:46:16 +0100

    new commit

--- event_management_system_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EventManagementSystemAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'event_management_system_app'
